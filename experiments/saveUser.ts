505bf49c979894adf3fbba49287cd0ac771e0bb7
Maciek Kowalski <maciej.kowalski.mk@gmail.com>
2024-11-03 17:04:16 +0100

    save user in the postgres

--- experiments/saveUser.ts
+++ experiments/saveUser.ts
@@ -2,27 +2,29 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const generateRandomUser = () => {
+  return {
+    email: `user-${Math.random()}@example.com`,
+    name: `User ${Math.random()}`,
+  };
+};
+
 async function main() {
   console.log("--- start create user");
   const user = await prisma.user.create({
-    data: {
-      email: "alice@example.com",
-      name: "Alice",
-    },
+    data: generateRandomUser(),
   });
   console.log("Created user:", user);
 }
 
-await main();
-
-// main()
-//   .catch((e) => {
-//     console.error(e);
-//     process.exit(1);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+await main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+  });
 
 async function listUsers() {
   console.log("--- start list users");
