98a485b2557cc76577311b54454b2364f2881912
Maciek Kowalski <maciej.kowalski.mk@gmail.com>
2024-11-23 15:12:45 +0100

    remove

--- experiments/fireCrawl.ts
+++ experiments/fireCrawl.ts
@@ -3,7 +3,7 @@ const apiKey = Bun.env.FIRECRAWL_API_KEY;
 // Install with npm install @mendable/firecrawl-js
 import FireCrawlApp from "@mendable/firecrawl-js";
 
-const app = new FireCrawlApp({ apiKey: "fc-b6d2d7caccc2441c82f528e84ee94936" });
+const app = new FireCrawlApp({ apiKey });
 
 const scrapeResult = await app.scrapeUrl("https://gazeta.pl/", {
   formats: ["markdown"],
